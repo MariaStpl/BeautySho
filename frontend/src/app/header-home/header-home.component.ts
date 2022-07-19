6d95366b4a6281878a0a215767a232fd6d5cb456
MariaStpl <mariaeunike01@gmail.com>
2022-07-19 08:26:57 +0700

    feat(profil): get profil by id user

--- frontend/src/app/header-home/header-home.component.ts
+++ frontend/src/app/header-home/header-home.component.ts
@@ -18,120 +18,101 @@ import { SignupComponent } from '../signup/signup.component';
 @Component({
   selector: 'app-header-home',
   templateUrl: './header-home.component.html',
-  styleUrls: ['./header-home.component.scss']
+  styleUrls: ['./header-home.component.scss'],
 })
 export class HeaderHomeComponent implements OnInit {
-    public totalItem: number = 0;
-    public hasil:any;
-    public profilId:any;
-    public responseMessage:any;
+  public totalItem: number = 0;
+  public hasil: any;
+  public profilId: any;
+  public responseMessage: any;
 
+  constructor(
+    private dialog: MatDialog,
+    private router: Router,
+    private userService: UserService,
+    private cartService: CartService,
+    private ngxService: NgxUiLoaderService,
+    private profilService: ProfilService,
+    private route: ActivatedRoute,
+    private snackbarService: SnackbarService
+  ) {}
 
-    constructor(private dialog: MatDialog,
-        private router: Router,
-        private userService: UserService,
-        private cartService:CartService,
-        private ngxService:NgxUiLoaderService,
-        private profilService:ProfilService,
-        private route: ActivatedRoute,
-        private snackbarService:SnackbarService) { }
+  ngOnInit(): void {
+    this.token();
+    this.cartService.getCart().subscribe((response: any) => {
+      this.totalItem = response.length;
+    });
+  }
 
-    ngOnInit(): void {
-        this.route.params.subscribe(params => {
-            console.log(params) //log the entire params object
-            console.log(params['id'])
-            this.profilDisplay(params.id)
-        }) 
-        this.token()
-        this.cartService.getCart().subscribe((response: any) => {
-            this.totalItem = response.length;
-            
-        })
-    }
+  profilView() {
+    this.userService.checkToken().subscribe(
+      (response: any) => {
+        this.router.navigate(['/profil/get/' + response.id]);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
 
-    profilDisplay(id:any) {
-        this.profilService.get(id).subscribe((response: any) => {
-            this.ngxService.stop();
-            this.profilId = response.map((data: any) => {
-                
-                return data
-            })   
-        }, (error: any) => {
-            this.ngxService.stop();
-            console.log(error);
-            if (error.error?.message) {
-                this.responseMessage = error.error?.message;
-            }
-            else {
-                this.responseMessage = GlobalConstants.genericError;
-            }
-            this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-        })
-        console.log(this.profilId);
-    }
-
-    profilView(id:any){
-        this.router.navigate(['/profil/get/'+id])
-    }
-
-    token(){
-        if (localStorage.getItem('token') != null) {
-            this.userService.checkToken().subscribe((response: any) => {
-                this.hasil = response.message  
-            }, (error: any) => {
-                console.log(error);
-            }
-            )
+  token() {
+    if (localStorage.getItem('token') != null) {
+      this.userService.checkToken().subscribe(
+        (response: any) => {
+          this.hasil = response;
+          this.profilId = response.id;
+          console.log(this.hasil);
+        },
+        (error: any) => {
+          console.log(error);
         }
-        
+      );
     }
+  }
 
-    signupAction() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = "550px";
-        this.dialog.open(SignupComponent, dialogConfig);
+  signupAction() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(SignupComponent, dialogConfig);
+  }
+  forgotPasswordAction() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(ForgotPasswordComponent, dialogConfig);
+  }
+  loginAction() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(LoginComponent, dialogConfig);
+    this.token();
+  }
 
-    }
-    forgotPasswordAction() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = "550px";
-        this.dialog.open(ForgotPasswordComponent, dialogConfig);
+  cartAction() {
+    this.router.navigate(['/cart']);
+  }
 
-    }
-    loginAction() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = "550px";
-        this.dialog.open(LoginComponent, dialogConfig);
-        this.token()
-
-    }
-
-    cartAction() {;
-        this.router.navigate(['/cart'])
-    }
-
-    shipAction() {
-        this.router.navigate(['/order/getAll'])
-    }
-    logout() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.data = {
-            message: 'Logout'
-        };
-        const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
-        const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((user) => {
-            dialogRef.close();
-            localStorage.clear();
-            this.router.navigate(['/']);
-            this.token()
-        })
-    }
-    changePassword() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = "550px";
-        this.dialog.open(ChangePasswordComponent, dialogConfig);
-    }
-
+  shipAction() {
+    this.router.navigate(['/order/getAll']);
+  }
+  logout() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      message: 'Logout',
+    };
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe(
+      (user) => {
+        dialogRef.close();
+        localStorage.clear();
+        this.router.navigate(['/']);
+        window.location.reload();
+        this.token();
+      }
+    );
+  }
+  changePassword() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(ChangePasswordComponent, dialogConfig);
+  }
 }
-
-
--- frontend/src/app/layouts/full/header/header.component.ts
+++ frontend/src/app/layouts/full/header/header.component.ts
@@ -9,83 +9,91 @@ import { SnackbarService } from 'src/app/services/snackbar.service';
 import { GlobalConstants } from 'src/app/shared/global-constants';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: []
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [],
 })
 export class AppHeaderComponent {
-    public totalItem: number = 0;
-    role: any;
-    public notifList:any;
-    public responseMessage:any;
+  public totalItem: number = 0;
+  role: any;
+  public notifList: any;
+  public responseMessage: any;
 
+  constructor(
+    private router: Router,
+    private dialog: MatDialog,
+    private notificationService: NotificationService,
+    private snackbarService: SnackbarService
+  ) {
+    this.openNotif();
+  }
 
-
-    constructor(private router: Router,
-        private dialog: MatDialog,
-        private notificationService: NotificationService,
-        private snackbarService: SnackbarService) {
-            this.openNotif()        
-    }
-    
-    openNotif() {
-        this.notificationService.get().subscribe((response: any) => {
-            this.totalItem = 0; 
-            this.notifList = response.map((data: any) => {
-                console.log(data.statusNotif);
-                if (data.statusNotif === 'Unread') {
-                    this.totalItem =  this.totalItem + 1
-                }
-                return data
-            })
-        }, (error: any) => {
-            console.log(error);
-            if (error.error?.message) {
-                this.responseMessage = error.error?.message;
-            }
-            else {
-                this.responseMessage = GlobalConstants.genericError;
-            }
-            this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-        })
-    }
-    
-    detailOrder(checkoutId:any){
-        var data ={
-            checkoutId:checkoutId
+  openNotif() {
+    this.notificationService.get().subscribe(
+      (response: any) => {
+        this.totalItem = 0;
+        this.notifList = response.map((data: any) => {
+          console.log(data.statusNotif);
+          if (data.statusNotif === 'Unread') {
+            this.totalItem = this.totalItem + 1;
+          }
+          return data;
+        });
+      },
+      (error: any) => {
+        console.log(error);
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
         }
-        this.notificationService.update(data).subscribe((response:any)=>{
-            this.router.navigate(['/beautyshop/orders']);
-        }, (error:any)=>{
-            if (error.error?.message) {
-                this.responseMessage = error.error?.message;
-            }
-            else {
-                this.responseMessage = GlobalConstants.genericError;
-            }
-            this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-        })
-       
-    }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
 
+  detailOrder(checkoutId: any) {
+    var data = {
+      checkoutId: checkoutId,
+    };
+    this.notificationService.update(data).subscribe(
+      (response: any) => {
+        this.router.navigate(['/beautyshop/orders']);
+      },
+      (error: any) => {
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
 
-
-    logout() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.data = {
-            message: 'Logout'
-        };
-        const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
-        const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((user) => {
-            dialogRef.close();
-            localStorage.clear();
-            this.router.navigate(['/administrator']);
-        })
-    }
-    changePassword() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = "550px";
-        this.dialog.open(ChangePasswordComponent, dialogConfig);
-    }
-
+  logout() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      message: 'Logout',
+    };
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe(
+      (user) => {
+        dialogRef.close();
+        localStorage.clear();
+        this.router.navigate(['/administrator']);
+      }
+    );
+  }
+  changePassword() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(ChangePasswordComponent, dialogConfig);
+  }
 }
