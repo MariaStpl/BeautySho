67bbea5ae993076b5d6037c04d54c2b118b50332
MariaStpl <mariaeunike01@gmail.com>
2022-07-21 16:51:07 +0700

    feat(profil) : display & styling photo profil

--- frontend/src/app/profil/profil.component.ts
+++ frontend/src/app/profil/profil.component.ts
@@ -4,51 +4,79 @@ import { ProfilService } from '../services/profil.service';
 import { SnackbarService } from '../services/snackbar.service';
 import { GlobalConstants } from '../shared/global-constants';
 import { environment } from 'src/environments/environment';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+import { EditProfilComponent } from '../edit-profil/edit-profil.component';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.scss']
+  styleUrls: ['./profil.component.scss'],
 })
 export class ProfilComponent implements OnInit {
-    responseMessage: any;
-    public profilView:any;
+  responseMessage: any;
+  public profilView: any;
 
-  constructor(private profilService:ProfilService,
-    private ngxService:NgxUiLoaderService,
+  constructor(
+    private profilService: ProfilService,
+    private ngxService: NgxUiLoaderService,
     private route: ActivatedRoute,
-    private snackbarService:SnackbarService) { }
+    private snackbarService: SnackbarService,
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-        console.log(params) //log the entire params object
-        console.log(params['id'])
-        this.profilDisplay(params.id)
-    }) 
+    this.route.params.subscribe((params) => {
+      console.log(params); //log the entire params object
+      console.log(params['id']);
+      this.profilDisplay(params.id);
+    });
   }
 
   profilDisplay(id: any) {
-    this.profilService.get(id).subscribe((response: any) => {
+    this.profilService.get(id).subscribe(
+      (response: any) => {
         this.ngxService.stop();
         this.profilView = response.map((data: any) => {
-            // if (data.itemImage) {
-            //     data.itemImage = environment.apiUrl + '/images/upload_detail/' + data.itemImage;
-            //     return data;
-            // }
+            if (data.profil_image) {
+                data.profil_image = environment.apiUrl + '/images/upload_images/' + data.profil_image;
+            }
             return data
-        })
-    }, (error: any) => {
+        });
+      },
+      (error: any) => {
         this.ngxService.stop();
         console.log(error);
         if (error.error?.message) {
-            this.responseMessage = error.error?.message;
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
         }
-        else {
-            this.responseMessage = GlobalConstants.genericError;
-        }
-        this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-    })
-}
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
 
+  editProfil(values: any) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'Edit',
+      data: values,
+    };
+    dialogConfig.width = '850px';
+    const dialogRef = this.dialog.open(EditProfilComponent, dialogConfig);
+    this.router.events.subscribe(() => {
+      dialogRef.close();
+    });
+    const sub = dialogRef.componentInstance.onEditProduct.subscribe(
+      (response) => {
+        this.ngOnInit();
+      }
+    );
+  }
 }
