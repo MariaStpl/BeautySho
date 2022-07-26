76a25b7e0f6eefcd80d1b91e73d47261225ed04f
MariaStpl <mariaeunike01@gmail.com>
2022-07-26 08:21:13 +0700

    fix(menu-item): delete menu item

--- frontend/src/app/shared/menu-items.ts
+++ frontend/src/app/shared/menu-items.ts
@@ -12,9 +12,9 @@ const MENUITEMS = [
     { state: 'category', name: 'Manage Category', icon: 'category', role: 'admin' },
     { state: 'product', name: 'Manage Product', icon: 'inventory_2', role: 'admin' },
     { state: 'detail', name: 'Detail Product', icon: 'inventory_1', role: 'admin' },
-    { state: 'order', name: 'Manage Order', icon: 'list_alt', role: '' },
+    //{ state: 'order', name: 'Manage Order', icon: 'list_alt', role: '' },
     { state: 'orders', name: 'Orders', icon: 'receipt', role: 'admin' },
-    { state: 'bill', name: 'View Bill', icon: 'receipt', role: '' },
+    //{ state: 'bill', name: 'View Bill', icon: 'receipt', role: '' },
     { state: 'user', name: 'View User', icon: 'people', role: 'admin' },
     
     
