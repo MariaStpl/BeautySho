79f11b1be2e37e45cea242016f6b79cadb7cf42b
MariaStpl <mariaeunike01@gmail.com>
2022-07-26 08:22:33 +0700

    feat(dashboard): create new table

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ViewBillOrdersComponent } from '../material-component/dialog/view-bill-orders/view-bill-orders.component';
 import { DashboardService } from '../services/dashboard.service';
 import { SnackbarService } from '../services/snackbar.service';
 import { GlobalConstants } from '../shared/global-constants';
@@ -10,16 +14,27 @@ import { GlobalConstants } from '../shared/global-constants';
 	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements AfterViewInit {
+    displayedColumns: string[] = ['no','name', 'contactNumber', 'shipping_option','receipt','status', 'edit'];
+    displayedColumnsProduct: string[] = ['no','name', 'total'];
+    displayedColumnsItem: string[] = ['no','productName', 'item','total'];
+    dataSource: any;
+    dataSource1: any;
+    dataSource2: any;
     responseMessage:any;
     data:any;
 
 	ngAfterViewInit() { }
 
 	constructor(private dashboardService:DashboardService,
-        private ngxService:NgxUiLoaderService,
-        private snackbarService:SnackbarService) {
+        private ngxService: NgxUiLoaderService,
+        private dialog: MatDialog,
+        private snackbarService: SnackbarService,
+        private router: Router) {
             this.ngxService.start();
             this.dashboardData();
+            this.tableData();
+            this.tableTopProduct();
+            this.tableTopItem()
 	}
     dashboardData(){
         this.dashboardService.getDetails().subscribe((response:any)=>{
@@ -37,4 +52,69 @@ export class DashboardComponent implements AfterViewInit {
             this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
         })
     }
+    
+    tableTopProduct() {
+        this.dashboardService.topProduct().subscribe((response: any) => {
+            this.ngxService.stop();
+            this.dataSource1 = new MatTableDataSource(response)
+        }, (error: any) => {
+            this.ngxService.stop();
+            console.log(error);
+            if (error.error?.message) {
+                this.responseMessage = error.error?.message;
+            }
+            else {
+                this.responseMessage = GlobalConstants.genericError;
+            }
+            this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+        })
+    }
+
+    tableTopItem() {
+        this.dashboardService.topItem().subscribe((response: any) => {
+            this.ngxService.stop();
+            this.dataSource2 = new MatTableDataSource(response)
+        }, (error: any) => {
+            this.ngxService.stop();
+            console.log(error);
+            if (error.error?.message) {
+                this.responseMessage = error.error?.message;
+            }
+            else {
+                this.responseMessage = GlobalConstants.genericError;
+            }
+            this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+        })
+    }
+    
+    tableData() {
+        this.dashboardService.getOrders().subscribe((response: any) => {
+            this.ngxService.stop();
+            this.dataSource = new MatTableDataSource(response)
+        }, (error: any) => {
+            this.ngxService.stop();
+            console.log(error);
+            if (error.error?.message) {
+                this.responseMessage = error.error?.message;
+            }
+            else {
+                this.responseMessage = GlobalConstants.genericError;
+            }
+            this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+        })
+    }
+
+    handleViewAction(values:any){
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.data = {
+            data:values
+        };
+        dialogConfig.width="100%";
+        const dialogRef = this.dialog.open(ViewBillOrdersComponent, dialogConfig);
+        this.router.events.subscribe(()=>{
+            dialogRef.close();
+        })
+    }
+    
+
 }
