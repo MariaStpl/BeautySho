999d21ca6726b726d797bcf3834e85c87b0b1841
MariaStpl <mariaeunike01@gmail.com>
2022-07-27 17:02:15 +0700

    feat(notif): add limit data

--- Backend BeautyShop/routes/notification.js
+++ Backend BeautyShop/routes/notification.js
@@ -5,7 +5,7 @@ var auth = require('../services/authentication');
 var checkRole = require('../services/checkRole');
 
 router.get('/get',(req, res, next)=>{
-    var query = "select n.id as notifId, n.notif, n.orderTime, n.checkoutId, n.status as statusNotif, c.id, c.name as name, c.email as email, c.contactNumber, c.paymentMethod, c.address, c.shipping_option, c.status, c.orderTime, c.shipTime, c.completedTime from notification as n INNER JOIN checkout as c ON n.checkoutId = c.id order by n.id DESC";
+    var query = "select n.id as notifId, n.notif, n.orderTime, n.checkoutId, n.status as statusNotif, c.id, c.name as name, c.email as email, c.contactNumber, c.paymentMethod, c.address, c.shipping_option, c.status, c.orderTime, c.shipTime, c.completedTime from notification as n INNER JOIN checkout as c ON n.checkoutId = c.id order by n.id DESC limit 10";
     connection.query(query, (err, results)=>{
         if(!err){
             return res.status(200).json(results);
